8d5d95edaa82b5c84c7491a7aedbef3c10123346
Davi Silva <davisilvaphoto@gmail.com>
2023-04-26 08:32:06 -0300

    test: userlist - users

--- 02-users/src/UserList.test.js
+++ 02-users/src/UserList.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import UserList from './UserList';
 
 test('should render one row per user', () => {
@@ -15,4 +15,19 @@ test('should render one row per user', () => {
   expect(rows).toHaveLength(2);
 });
 
-test('should render the email and name of each user', () => {});
+test('should render the email and name of each user', () => {
+  const users = [
+    { name: 'Jonh Doe', email: 'jonh_doe@email.com' },
+    { name: 'Jane Doe', email: 'jane_doe@email.com' },
+  ];
+
+  render(<UserList users={users} />);
+
+  for (let user of users) {
+    const name = screen.getByRole('cell', { name: user.name });
+    const email = screen.getByRole('cell', { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
+});
