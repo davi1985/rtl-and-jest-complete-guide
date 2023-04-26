d2a64e40889027b7eb4d4eb1f7b82f7a1d986c8d
Davi Silva <davisilvaphoto@gmail.com>
2023-04-26 08:24:51 -0300

    test: userlist - get elements with container

--- 02-users/src/UserList.test.js
+++ 02-users/src/UserList.test.js
@@ -1,18 +1,17 @@
-import { render, screen, within } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import UserList from './UserList';
 
 test('should render one row per user', () => {
-  //render component
   const users = [
     { name: 'Jonh Doe', email: 'jonh_doe@email.com' },
     { name: 'Jane Doe', email: 'jane_doe@email.com' },
   ];
-  render(<UserList users={users} />);
 
-  // find all the rows in the table
-  const rows = within(screen.getByTestId('users')).getAllByRole('row');
+  const { container } = render(<UserList users={users} />);
 
-  // assertion: correct number of rows in the table
+  // eslint-disable-next-line
+  const rows = container.querySelectorAll('tbody tr');
+
   expect(rows).toHaveLength(2);
 });
 
