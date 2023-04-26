8a5cda83a4bcf339d075a85e538cf0636dfcb1ed
Davi Silva <davisilvaphoto@gmail.com>
2023-04-26 07:55:55 -0300

    test: useform - fix inputs with labels

--- 02-users/src/UserForm.test.js
+++ 02-users/src/UserForm.test.js
@@ -17,7 +17,8 @@ test('it calls onUserAdd when the form is submitted', () => {
   const mock = jest.fn();
   render(<UserForm onUserAdd={mock} />);
 
-  const [nameInput, emailInput] = screen.getAllByRole('textbox');
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
 
   userEvent.click(nameInput);
   userEvent.keyboard('Jonh Doe');
